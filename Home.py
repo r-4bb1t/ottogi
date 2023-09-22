963c4a7ddca975c5330bed81034b9f09c7b9d11e
Julia Biolata <85150543+julia-biolat@users.noreply.github.com>
2023-09-23 03:38:46 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,66 +1,44 @@
 import streamlit as st
 import pandas as pd
-import streamlit as st
-import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-# CSV 파일에서 음식 데이터 읽어오기
-food_data_df = pd.read_csv('전국통합식품영양성분정보(음식).csv', encoding='cp949')
-food_data = food_data_df.set_index('식품명').T.to_dict()
+# 그래프 생성
+def display_graph():
+    global carbs, fats, proteins  # 이 부분 추가
 
+    # 데이터 수집
+    calories = sum(st.session_state.get(meal + "_calories", 0) for meal in ["아침", "점심", "저녁"])
+    carbs = sum(food_data[food]["탄수화물(g)"] for meal in ["아침", "점심", "저녁"] for food in st.session_state.get(meal + "_foods", []))
+    fats = sum(food_data[food]["총 지방(g)"] for meal in ["아침", "점심", "저녁"] for food in st.session_state.get(meal + "_foods", []))
+    proteins = sum(food_data[food]["단백질(g)"] for meal in ["아침", "점심", "저녁"] for food in st.session_state.get(meal + "_foods", []))
+    
+    nutrients = ["탄수화물(g)", "단백질(g)", "지방(g)"]
+    intake = {nutrient: 0 for nutrient in nutrients}
+    
+    for meal_type in ["아침", "점심", "저녁"]:
+        for food in st.session_state.food_list[meal_type]:
+            for nutrient in nutrients:
+                intake[nutrient] += food_data[food][nutrient]
+    
+    df = pd.DataFrame(intake, index=["Intake"])
+    st.bar_chart(df.T)  # Transpose the dataframe for better visualization
+
+
+# 섭취한 음식 리스트 나열
+def display_food_list():
+    all_foods = []
+    for meal_type in ["아침", "점심", "저녁"]:
+        all_foods.extend(st.session_state.food_list[meal_type])
+    
+    st.write("### 오늘 섭취한 음식:")
+    for food in all_foods:
+        st.write(f"* {food}  {food_data[food]['에너지(kcal)']}kcal")
 
 # 표준체중 계산 함수
 def calculate_weightav(height):
     result = height * height * 0.0021
     return result
-
-st.image("logo.png", width=100) 
-
-
-# 예시 데이터
-data = {
-    "음식 이름": ["칰햄 돈부리", "닭고기크림스튜", "카레 치즈라면", "참치두부유부초밥"],
-    "칼로리(kcal)": [699.4, 573.24, 337.2, 338.3],
-    "탄수화물(g)": [94.1, 37.61, 102.96, 10.7],
-    "단백질(g)": [33.0, 41.08, 20.54, 28.5],
-    "지방(g)": [22.2, 29.02, 24.84, 20.1]
-}
-df = pd.DataFrame(data)
-
-
-col1, col2 = st.columns([1, 6]) 
-col1.image("ch2.png", width=110)  # width를 조정하여 아이콘 크기를 변경할 수 있습니다.
-
-# 두 번째 컬럼에 제목 추가
-col2.title("적정 칼로리 및 식단 관리")
-
-# 키와 몸무게 입력
-height = st.text_input("키를 입력해주세요 (cm):")
-if height:
-    st.session_state.height = height
-weight = st.text_input("몸무게를 입력해주세요 (kg):")
-
-
-reccal = 0
-if height and weight:
-    height = float(height)
-    weight = float(weight)
-    
-    weightav = calculate_weightav(height)
-    
-    # 내용을 숨기거나 표시하는 박스 생성
-    with st.expander("결과 보기"):
-        st.markdown(f"**평균체중**: 키가 {height}cm인 경우 평균체중은: {weightav:.2f}kg 입니다.")
-        
-        # 권장칼로리 계산
-        reccal = weightav * 30
-        st.markdown(f"**권장칼로리**: {reccal:.2f}Kcal")
-        
-        # 체중감량 권장 칼로리 계산
-        dietcal = reccal - 500
-        st.markdown(f"**체중감량칼로리**: {dietcal:.2f}")
-
-
-
 
 # 음식 검색 및 추가
 def search_food(session_state, meal_type):
@@ -72,11 +50,80 @@ def search_food(session_state, meal_type):
         session_state.total_calories += food_data[food_name]["에너지(kcal)"]  # 수정된 부분
         session_state.food_list[meal_type].append(food_name)
 
+# 섭취 가능한 칼로리와 가장 가까운 제품 5개 추천
+def recommend_foods():
+    ottogi_data_df['distance'] = np.sqrt(
+        (ottogi_data_df['에너지(kcal)'] - remaining_calories)**2 +
+        (ottogi_data_df['탄수화물(g)'] - remaining_carbs)**2 +
+        (ottogi_data_df['단백질(g)'] - remaining_proteins)**2 +
+        (ottogi_data_df['지방(g)'] - remaining_fats)**2
+    )
+    recommended = ottogi_data_df.nsmallest(5, 'distance')
+    st.write("추천 음식:")
+    st.write(recommended)
+
+
+################################################
+
+st.image("logo.png", width=100) 
+
+dietcal = 0
+carbo = 0
+pro = 0
+fat = 0
+# CSV 파일에서 음식 데이터 읽어오기
+food_data_df = pd.read_csv('전국통합식품영양성분정보(음식).csv', encoding='cp949')
+food_data = food_data_df.set_index('식품명').T.to_dict()
+
+
+col1, col2 = st.columns([1, 6]) 
+col1.image("ch2.png", width=110)  # width를 조정하여 아이콘 크기를 변경할 수 있습니다.
+
+# 두 번째 컬럼에 제목 추가
+col2.title("적정 칼로리 및 식단 관리")
+
+# 키와 몸무게 입력
+height = st.text_input("키를 입력해주세요 (cm):")
+weight = st.text_input("몸무게를 입력해주세요 (kg):")
+
+reccal = 0
+if height and weight:
+    height = float(height)
+    weight = float(weight)
+    
+    weightav = calculate_weightav(height)
+    
+    # 내용을 펼쳐진 상태로 보여주는 박스 생성
+    with st.expander("결과 보기", expanded=True):  # expanded=True 추가
+        st.markdown(f"**평균체중**: {weightav:.2f}kg")
+        
+        # 권장칼로리 계산
+        reccal = weightav * 30
+        st.markdown(f"**권장칼로리**: {reccal:.2f}Kcal")
+        
+        # 체중감량 권장 칼로리 계산
+        dietcal = reccal - 500
+        st.markdown(f"**체중감량칼로리**: {dietcal:.2f}Kcal")
+
+        # 권장 탄단지 계산
+        carbo = reccal * 0.5 / 4
+        st.markdown(f"**권장 탄수화물**: {carbo:.2f}g")
+        
+        pro = reccal * 0.3 / 4
+        st.markdown(f"**권장 단백질**: {pro:.2f}g")
+        
+        fat = reccal * 0.2 / 9
+        st.markdown(f"**권장 지방**: {fat:.2f}g")
+
+
+
+
 # 세션 상태 초기화
 if not hasattr(st.session_state, 'food_list'):
     st.session_state.food_list = {"아침": [], "점심": [], "저녁": []}
 if not hasattr(st.session_state, 'total_calories'):
     st.session_state.total_calories = 0
+
 
 # 아침, 점심, 저녁 음식 검색 및 추가
 for meal_type in ["아침", "점심", "저녁"]:
@@ -86,9 +133,21 @@ for meal_type in ["아침", "점심", "저녁"]:
 
 st.write(f"오늘 섭취한 총 칼로리: {st.session_state.total_calories}Kcal")
 
+#리스트
+display_food_list()
+#그래프
+display_graph()
+
 
 # 추천 시스템
-remaining_calories = reccal - st.session_state.total_calories  # 수정된 부분
-recommended_foods = df[df["칼로리(kcal)"] <= remaining_calories]
-st.write("추천 음식:")
-st.write(recommended_foods)
+remaining_calories = dietcal - st.session_state.total_calories
+remaining_carbs = carbo - carbs
+remaining_proteins = pro - proteins
+remaining_fats = fat - fats
+
+# CSV 파일에서 오뚜기 음식 데이터 읽어오기
+ottogi_data_df = pd.read_csv('오뚜기.csv', encoding='cp949')
+ottogi_data = ottogi_data_df.set_index('식품명').T.to_dict()  # '음식 이름' 대신 '식품명' 사용
+
+# 섭취 가능한 칼로리와 가장 가까운 제품 5개 추천
+recommend_foods()
