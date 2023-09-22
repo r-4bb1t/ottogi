9b11373e0464217613a7f01992d190f89d6e347b
r4bb1t <r4bb1t.github@gmail.com>
2023-09-23 02:45:56 +0900

    fix: fix recipe header

--- texts.py
+++ texts.py
@@ -1,9 +1,15 @@
-from utils.calories import calculate_cal
+from utils.calories import calculate_cal, calculate_nut
 import csv
 import random
 
-def random_rows_from_csv(csv_filename, num_rows):
-    with open(csv_filename, 'r', newline='', encoding='utf-8') as csvfile:
+def get_header(file_name):
+    with open(file_name, 'r', newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        rows = list(reader)
+        return rows[0]
+
+def random_rows_from_csv(file_name, num_rows):
+    with open(file_name, 'r', newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
         rows = list(reader)
         if len(rows) > 1:
@@ -12,9 +18,14 @@ def random_rows_from_csv(csv_filename, num_rows):
     return None
 
 def random_rows_to_string(file_name):
+    headers = get_header(file_name)
     random_rows = random_rows_from_csv(file_name, 3)
     if random_rows:
-        return '\n'.join(['---\n'.join(row) for row in random_rows])
+        res = ''
+        for row in random_rows:
+            res += '\n'.join([(headers[i] + ": " + field) for i, field in enumerate(row)])
+            res += '\n----\n'
+        return res
     else:
         return ""
 
@@ -26,8 +37,8 @@ def recipes():
 def userinfo(height, today):
   return \
 '''너는 다이어트 코치 "마요"이다. 유저에게 어린아이같은 귀여운 말투를 사용해야 한다.
-내가 입력한 나의 정보에 맞게, 위 레시피 중 적절한 레시피를 추천하고 레시피를 추천한 이유, 자세한 설명과 함께 조언해야 한다.
-나의 키와 체중에 맞는 권장 칼로리는 {cal}kcal이며, 오늘 섭취한 칼로리는 {today}kcal이다.
-'''.format(cal=calculate_cal(height), today=today)
+내가 입력한 나의 필요 칼로리에 맞게, 위 레시피 중 적절한 레시피를 추천하고 레시피를 추천한 이유, 자세한 설명과 함께 조언해야 한다.
+나의 키와 체중에 맞는 권장 칼로리는 {cal}kcal이며, 필요한 영양 정보는 {nut} 오늘 섭취한 칼로리는 {today}kcal이다.
+'''.format(cal=calculate_cal(height), nut=calculate_nut(height), today=today)
 
 res = '''마크다운 형식으로 답변하되, 각 레시피의 URL과 이미지를 표시하라.'''
--- utils/calories.py
+++ utils/calories.py
@@ -8,3 +8,6 @@ def calculate_bmi(height):
 # 권장칼로리
 def calculate_cal(height):
     return calculate_bmi(height) * 30 - 50
+
+def calculate_nut(height):
+    return '탄수화물 {car}g, 지방 {fat}g, 단백질 {protein}g'.format(car=calculate_cal(height)*(5/13), fat=calculate_cal(height)*(2/13), protein=calculate_cal(height)*(5/13))
