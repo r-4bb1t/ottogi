cc83f0ab59b592f2a18846e40d094e0e4d2fc1c0
r4bb1t <r4bb1t.github@gmail.com>
2023-09-23 03:39:10 +0900

    feat: prompt

--- utils/calories.py
+++ utils/calories.py
@@ -3,11 +3,11 @@ import math
 # 표준체중(BMI)
 def calculate_bmi(height):
     result = height * height * 0.0021
-    return math.floor(result)
+    return result
 
 # 권장칼로리
 def calculate_cal(height):
-    return calculate_bmi(height) * 30 - 50
+    return math.floor(calculate_bmi(height) * 30 - 50)
 
 def calculate_nut(height):
-    return '탄수화물 {car}g, 지방 {fat}g, 단백질 {protein}g'.format(car=calculate_cal(height)*(5/13), fat=calculate_cal(height)*(2/13), protein=calculate_cal(height)*(5/13))
+    return '탄수화물 {car}g, 지방 {fat}g, 단백질 {protein}g'.format(car=calculate_cal(height)*(5/13)/4, fat=calculate_cal(height)*(2/13)/4, protein=calculate_cal(height)*(5/13)/9)
